aba1d2eaf4dce32d587a241de25b96c91d45459e
alexnakorn <alexnakorn@gmail.com>
2013-04-15 13:41:11 -0700

    Update QuestionTable.java
    White spaces & private to public

--- src/com/appjam/team16/db/QuestionTable.java
+++ src/com/appjam/team16/db/QuestionTable.java
@@ -4,24 +4,24 @@ public class QuestionTable {
 	
 	public static final String TABLE_NAME 				= "Question";
 	
-	private static final String COLUMN_ID 				= "_id";
-	private static final String COLUMN_TITLE 			= "title";
-	private static final String COLUMN_ANSWER_TYPE 		= "answer_type";
-	private static final String COLUMN_QUESTION_TEXT	= "question_text";
-	private static final String COLUMN_QUESTION_AUDIBLE	= "question_audible";
-	private static final String COLUMN_CREATE_TIMESTAMP	= "create_timestamp";
-	private static final String COLUMN_MODIFY_TIMESTAMP	= "modify_timestamp";
+	public static final String COLUMN_ID 				= "_id";
+	public static final String COLUMN_TITLE 			= "title";
+	public static final String COLUMN_ANSWER_TYPE 		= "answer_type";
+	public static final String COLUMN_QUESTION_TEXT	= "question_text";
+	public static final String COLUMN_QUESTION_AUDIBLE	= "question_audible";
+	public static final String COLUMN_CREATE_TIMESTAMP	= "create_timestamp";
+	public static final String COLUMN_MODIFY_TIMESTAMP	= "modify_timestamp";
 	
 	// TODO: Need column types + constraints
 	public static final String TABLE_CREATE_STATEMENT =
 		"CREATE TABLE " + TABLE_NAME + "(" + 
-		COLUMN_ID 				+ " INTEGER AUTOINCREMENT NOT NULL" +
-		COLUMN_TITLE 			+ " TEXT NOT NULL" +
-		COLUMN_ANSWER_TYPE 		+ " INTEGER NOT NULL" +
-		COLUMN_QUESTION_TEXT 	+ " TEXT" +
-		COLUMN_QUESTION_AUDIBLE + " TEXT" +
-		COLUMN_CREATE_TIMESTAMP	+ " TEXT NOT NULL" +
-		COLUMN_MODIFY_TIMESTAMP	+ " TEXT NOT NULL" +
-		" PRIMARY KEY (" + COLUMN_ID + ")"
+		COLUMN_ID 				+ " INTEGER AUTOINCREMENT NOT NULL " +
+		COLUMN_TITLE 			+ " TEXT NOT NULL " +
+		COLUMN_ANSWER_TYPE 		+ " INTEGER NOT NULL " +
+		COLUMN_QUESTION_TEXT 	+ " TEXT " +
+		COLUMN_QUESTION_AUDIBLE + " TEXT " +
+		COLUMN_CREATE_TIMESTAMP	+ " TEXT NOT NULL " +
+		COLUMN_MODIFY_TIMESTAMP	+ " TEXT NOT NULL " +
+		" PRIMARY KEY (" + COLUMN_ID + ")" +
 		");";
 }
