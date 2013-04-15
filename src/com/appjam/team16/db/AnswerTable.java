73a3e595e74a2efde1bd3ff229bdab9fd4e1875b
alexnakorn <alexnakorn@gmail.com>
2013-04-15 13:38:19 -0700

    Update AnswerTable.java

--- src/com/appjam/team16/db/AnswerTable.java
+++ src/com/appjam/team16/db/AnswerTable.java
@@ -4,19 +4,21 @@ public class AnswerTable {
 	
 	public static final String TABLE_NAME 				= "Answer";
 	
-	private static final String COLUMN_ID 				= "_id";
-	private static final String COLUMN_QUESTION_ID		= "question_id";
-	private static final String COLUMN_CREATE_TIMESTAMP	= "create_timestamp";
-	private static final String COLUMN_RESPONSE_TIME	= "response_time";
-	private static final String COLUMN_ANSWER_VALUE		= "answer_value";
+	public static final String COLUMN_ID 				= "_id";
+	public static final String COLUMN_QUESTION_ID		= "question_id";
+	public static final String COLUMN_CREATE_TIMESTAMP	= "create_timestamp";
+	public static final String COLUMN_RESPONSE_TIME	= "response_time";
+	public static final String COLUMN_ANSWER_VALUE		= "answer_value";
 	
-	// TODO: Need column types + constraints
 	public static final String TABLE_CREATE_STATEMENT =
 		"CREATE TABLE " + TABLE_NAME + "(" + 
-		COLUMN_ID 				+ "" +
-		COLUMN_QUESTION_ID 		+ "" +
-		COLUMN_CREATE_TIMESTAMP + "" +
-		COLUMN_RESPONSE_TIME 	+ "" +
-		COLUMN_ANSWER_VALUE 	+ "" +
+		COLUMN_ID 				+ " INTEGER AUTOINCREMENT NOT NULL " +
+		COLUMN_QUESTION_ID 		+ " TEXT NOT NULL " +
+		COLUMN_CREATE_TIMESTAMP + " TEXT NOT NULL " +
+		COLUMN_RESPONSE_TIME 	+ " REAL NOT NULL " +
+		COLUMN_ANSWER_VALUE 	+ " INT NOT NULL " +
+		"FOREIGN KEY (" + COLUMN_QUESTION_ID + ") REFERENCES " + 
+			QuestionTable.TABLE_NAME + "(" + QuestionTable.COLUMN_ID + ") " +
+		"PRIMARY KEY (" + COLUMN_ID + ", " + COLUMN_QUESTION_ID + ")" +
 		");";
 }
