975c8cbd46ff07bd8e759fbd4fe05b6c0885bc19
thuytrangchung <02143060Tc>
2013-04-16 21:20:33 -0700

    display quizzes list in a dialog when "add to quiz" is clicked, I commented out the section that should be integrated with the database

--- src/com/appjam/team16/fragments/QuestionDetailFragment.java
+++ src/com/appjam/team16/fragments/QuestionDetailFragment.java
@@ -1,10 +1,14 @@
 package com.appjam.team16.fragments;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,8 +20,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.appjam.team16.R;
@@ -32,6 +38,24 @@ public class QuestionDetailFragment extends Fragment implements
 	}
 
 	public QuestionCreatedListener mCallback;
+	
+	private Button addToQuizButton;
+	//DATABASE ALERT
+	//dummy quizzes data array for "add to quiz button". You can initialize this by
+	//retrieving all the quizzes in the database
+	final CharSequence[] quizzes = {"Quiz 1", "Quiz 2", "Quiz 3", "Quiz 4", "Quiz 5"};
+	
+	//DATABASE ALERT
+	//keep track of which item has been chosen. Have to have the same size as the CharSequence[] above
+	//each question will have their own version of this variable, initialize this to all False
+	//boolean values will become true if the quizzes in their position is checked/selected
+	boolean[] checked = {false, false, false, false, false};
+	
+	//This ArrayList consists of the position of all the items that are checked
+	ArrayList<Integer> selectedItems = new ArrayList<Integer>(); 
+	
+	//this is used to display the quizzes that the question belongs to
+	private ListView quizList;
 
 	private EditText questionName;
 	private EditText questionQuestion;
@@ -39,7 +63,7 @@ public class QuestionDetailFragment extends Fragment implements
 	private Button recordAudioButton;
 	private Button removeAudioButton;
 	private Button playbackAudioButton;
-
+	
 	private boolean editingQuestion;
 
 	@Override
@@ -47,6 +71,74 @@ public class QuestionDetailFragment extends Fragment implements
 			Bundle savedInstanceState) {
 		View questionView = inflater.inflate(R.layout.question_detail,
 				container, false);
+		// Start "Add to quiz" button 
+		addToQuizButton = (Button) questionView
+				.findViewById(R.id.add_to_quiz_button);
+		addToQuizButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder quizList = new AlertDialog.Builder(
+						getActivity());
+				// keep track of selected Items
+				//mSelectedItems = new ArrayList<Integer>();
+
+				quizList.setTitle("Choose Quiz(es) to add this Question to")
+						
+						.setMultiChoiceItems(
+								quizzes,
+								checked,
+								new DialogInterface.OnMultiChoiceClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which, boolean isChecked) {
+										if (isChecked) {
+						                       // add checked item into selectedItem list
+						                       selectedItems.add(which);
+						                       checked[which] = true;
+						                   } else if (selectedItems.contains(which)) {
+						                       // Else, if the item is already in the array, remove it 
+						                       selectedItems.remove(Integer.valueOf(which));
+						                       checked[which] = false;
+						                   }
+										
+
+									}
+
+								})
+						.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								Toast.makeText(getActivity(), "Selection Saved", Toast.LENGTH_SHORT).show();
+								
+							}
+						})
+						.setCancelable(false);
+
+				quizList.create();
+				quizList.show();
+			}
+		});
+			
+		// End "Add to quiz button
+		
+		//DATABASE ALERT
+		// Add chosen quizzes to quizArray using .add()
+		// Start Quiz list display
+		// This show the quizArray contents to the list (in question_detail.xml)
+		
+		quizList = (ListView) questionView.findViewById(R.id.list);
+		ArrayAdapter<CharSequence> quizArray = new ArrayAdapter<CharSequence>(
+				getActivity(), android.R.layout.simple_list_item_1);
+		quizList.setAdapter(quizArray);
+				
+		// End Quiz list display
+		
+		
+		
+
 		questionName = (EditText) questionView
 				.findViewById(R.id.question_title);
 		questionQuestion = (EditText) questionView
@@ -75,13 +167,15 @@ public class QuestionDetailFragment extends Fragment implements
 		// the callback interface. If not, it throws an exception.
 		try {
 			mCallback = (QuestionCreatedListener) activity;
-		} catch (ClassCastException e) {
+		} catch (ClassCastException e) 
+		{
 			throw new ClassCastException(activity.toString()
 					+ " must implement QuestionCreatedListener");
 		}
 		Log.d("com.team16.appjam", "onAttach");
 		if (getArguments() != null
-				&& getArguments().containsKey(QuestionTable.COLUMN_ID)) {
+				&& getArguments().containsKey(QuestionTable.COLUMN_ID))
+		{
 			displayQuestion(getArguments().getLong(QuestionTable.COLUMN_ID));
 		}
 	}
@@ -91,7 +185,8 @@ public class QuestionDetailFragment extends Fragment implements
 		Context context = getActivity();
 		switch (v.getId()) {
 		case R.id.save_question_bttn:
-			if (editingQuestion) {
+			if (editingQuestion)
+			{
 				Toast.makeText(context, "Editing!", Toast.LENGTH_SHORT).show();
 				break;
 			}
