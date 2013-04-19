6e8cd951396acd1131894bb4f08393400aafa6e9
alexnakorn <alexnakorn@gmail.com>
2013-04-19 14:11:26 -0700

    Update CreateQuestionActivity.java

--- src/com/appjam/team16/CreateQuestionActivity.java
+++ src/com/appjam/team16/CreateQuestionActivity.java
@@ -57,6 +57,10 @@ public class CreateQuestionActivity extends SherlockFragmentActivity implements
 			intent = new Intent(this, ViewQuizzesActivity.class);
 			startActivity(intent);
 			break;
+		case R.id.see_answers_item:
+			intent = new Intent(this, SeeAnswersActivity.class);
+			startActivity(intent);
+			break;
 		default:
 			break;
 
